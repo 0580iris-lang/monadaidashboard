1d1cfcf5a406c9dd13a6f6458e44d1d0888290df
0580iris-lang <0580.iris@gmail.com>
2026-02-07 22:12:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from web3 import Web3
 import time
 
 # Monad public RPC endpoint
-RPC_URL = "https://rpc.ankr.com/monad"
+RPC_URL = "https://monad-mainnet.gateway.tatum.io"
 w3 = Web3(Web3.HTTPProvider(RPC_URL))
 
 # Your contracts
@@ -45,3 +45,4 @@ for name, address in contracts.items():
 # Auto-refresh
 time.sleep(10)
 st.rerun()
+
